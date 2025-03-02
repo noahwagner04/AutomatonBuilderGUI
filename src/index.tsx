882610e3e8bc6669402970a57a17ebe0fb64b574
Noah Wagner <wagnernoah@icloud.com>
2025-03-01 17:31:09 -0800

    Add keyboard shortcut for comment tool

--- src/index.tsx
+++ src/index.tsx
@@ -61,6 +61,8 @@ function App() {
         setCurrentTool(Tool.Transitions);
       } else if (ev.code === "KeyS") {
         setCurrentTool(Tool.Select);
+      } else if (ev.code === "KeyC") {
+        setCurrentTool(Tool.Comment);
       }
     });
   }, []);
